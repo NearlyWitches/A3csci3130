3133682534c8b9fa3d9d7c4b4b9f443d266e59e9
Dickie Bird <break0409@gmail.com>
2018-06-28 13:09:36 -0300

    UI testing and basic UI

--- app/src/androidTest/java/com/acme/a3csci3130/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/acme/a3csci3130/ExampleInstrumentedTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,6 +17,25 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void CreateTest(){
+        // type business info
+        onView(withId(R.id.number)).perform(typeText("123456789"));
+        onView(withId(R.id.name)).perform(typeText("Name Business"));
+        onView(withId(R.id.primary)).perform(typeText("Fisher"));
+        onView(withId(R.id.address)).perform(typeText("1111 North Street"));
+        onView(withId(R.id.province)).perform(typeText("NS"));
+
+        //create business
+        onView(withId(R.id.button)).perform(click());
+
+        // check the main screen
+        onView(withId(R.id.textView)).check(matches(withText("Business")));
+    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
